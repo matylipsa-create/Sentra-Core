fb8669572d463acfa4441c113affff2011f02df4
Lipari Matias Ariel <matylipsa@gmail.com>
2026-09-12 20:27:48 -0300

    Actualizar App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,5 @@
-import { useEffect } from "react";
-import { SentraVisionScreen } from "./components/SentraVisionScreen";
-import { deviceSensorManager } from "./core/DeviceSensorManager";
-import { sensorHub } from "./core/SensorHub";
-import { perceptionEngine } from "./core/PerceptionEngine";
-import { sensorModule } from "./modules/SensorModule";
-import { createSimulatedSensors } from "./sensors/SimulatedSensors";
-import { createNativeSensors } from "./services/NativeSensorService";
-import { createGPSSensor } from "./services/GPSSensorService";
-import { sentraGuardianHub } from "./core/SentraGuardianHub";
+import { BlindUnifiedUI } from './components/BlindUnifiedUI';
 
 export default function App() {
-  useEffect(() => {
-    deviceSensorManager.detectAvailableSensors();
-    sentraGuardianHub.initMultimodal();
-
-    for (const sensor of createNativeSensors()) {
-      try { sensorHub.register(sensor); } catch { /* id collision */ }
-    }
-    const gps = createGPSSensor();
-    if (gps) {
-      try { sensorHub.register(gps); } catch { /* id collision */ }
-    }
-    for (const sensor of createSimulatedSensors()) {
-      try { sensorHub.register(sensor); } catch { /* id collision */ }
-    }
-
-    perceptionEngine.start();
-    sensorModule.start();
-    sensorHub.startAll();
-
-    return () => {
-      sensorHub.stopAll();
-      perceptionEngine.stop();
-      sensorModule.stop();
-    };
-  }, []);
-
-  return <SentraVisionScreen />;
+  return <BlindUnifiedUI />;
 }
