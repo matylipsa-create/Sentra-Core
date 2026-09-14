c3bdcb75183129019de14ad3ff6206251528e249
barretomariabelen295-hash <barretomariabelen295@gmail.com>
2026-09-13 22:57:44 -0300

    Updated index.html

--- dist/assets/coco-ssd.es2017.esm.min-Ddmbo1FP.js
+++ dist/assets/coco-ssd.es2017.esm.min-Ddmbo1FP.js
@@ -1,4 +1,4 @@
-import{g as w,a as O,c as S}from"./index-Br8UL3CD.js";import{he as z,hF as q,i3 as M,i4 as P,iv as $,h6 as D,j2 as A}from"./version-Re-2TGz3.js";function B(f,u){for(var l=0;l<u.length;l++){const o=u[l];if(typeof o!="string"&&!Array.isArray(o)){for(const a in o)if(a!=="default"&&!(a in f)){const N=Object.getOwnPropertyDescriptor(o,a);N&&Object.defineProperty(f,a,N.get?N:{enumerable:!0,get:()=>o[a]})}}}return Object.freeze(Object.defineProperty(f,Symbol.toStringTag,{value:"Module"}))}/**
+import{g as w,a as O,c as S}from"./index-BpUgXyY5.js";import{he as z,hF as q,i3 as M,i4 as P,iv as $,h6 as D,j2 as A}from"./version-BhhW8wqP.js";function B(f,u){for(var l=0;l<u.length;l++){const o=u[l];if(typeof o!="string"&&!Array.isArray(o)){for(const a in o)if(a!=="default"&&!(a in f)){const N=Object.getOwnPropertyDescriptor(o,a);N&&Object.defineProperty(f,a,N.get?N:{enumerable:!0,get:()=>o[a]})}}}return Object.freeze(Object.defineProperty(f,Symbol.toStringTag,{value:"Module"}))}/**
  * @license
  * Copyright 2018 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
